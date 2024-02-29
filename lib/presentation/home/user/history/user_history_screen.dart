016fe5c5ae6f6ed3fe5aa18fadb9e2912eca4e87
mybuenoday <yoonjibiz@gmail.com>
2024-02-29 11:53:15 +0900

    feat: Gridview 높이 간격 조정 - image.network height: MediaQuery width*0.5

--- lib/presentation/home/user/history/user_history_screen.dart
+++ lib/presentation/home/user/history/user_history_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:go_router/go_router.dart';
+import 'package:my_flutter_image_searching_app_cleanarch/domain/model/photo/photo_model.dart';
 
 class UserHistoryScreen extends StatefulWidget {
   const UserHistoryScreen({super.key});
@@ -21,6 +21,8 @@ class _UserHistoryScreenState extends State<UserHistoryScreen> {
     'https://cdn.pixabay.com/photo/2021/04/24/09/51/checklist-6203690_1280.jpg',
     'https://cdn.pixabay.com/photo/2016/11/19/12/34/apple-1839046_1280.jpg',
   ];
+
+  late PhotoModel photo;
 
   bool _isSelectMode = false;
   List<bool> _selectedImageList = [];
@@ -61,110 +63,104 @@ class _UserHistoryScreenState extends State<UserHistoryScreen> {
                 if (!_isSelectMode) {
                   _selectedImageList =
                       List.generate(_imageLinks.length, (_) => false);
-                 /* _isLongPressed = false;
-                  _isLongPressedAfter = false;
-                  _selectedIndex = -1;*/
-                } /*
-                if (selectedImageCount > 0) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Delete($selectedImageCount)'),
-                      //duration: const Duration(seconds: 2),
-                    ),
-                  );
-                }*/
+                }
               },
             ),
           ),
         ],
       ),
-      body: Stack(
-        children: [
-          GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 2.0,
-            ),
-            itemCount: _imageLinks.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  if (_isSelectMode) {
-                    setState(() {
-                      _selectedImageList[index] = !_selectedImageList[index];
-                    });
-                  } else {
-                    context.push('/home');
-                  }
-                },
-                onLongPress: () {
-                  if (!_isSelectMode) {
-                    setState(() {
-                      _isSelectMode = true;
-                    });
-                  }
-                  setState(() {
-                    _isLongPressed = true;
-                    _selectedImageList =
-                        List.generate(_imageLinks.length, (_) => false);
-                    _selectedImageList[index] = true;
-                    _selectedIndex = index;
-                  });
-                },
-                child: Stack(
-                  alignment: Alignment.bottomRight,
-                  children: [
-                    Image.network(
-                      _imageLinks[index],
-                      fit: BoxFit.cover,
-                    ),
-                    if (_isSelectMode)
-                      Container(
-                        margin: const EdgeInsets.all(4.0),
-                        child: _selectedImageList[index]
-                            ? const Icon(
-                          Icons.check_circle,
-                          color: Colors.blueAccent,
-                          size: 24.0,
-                        )
-                            : const Icon(
-                          Icons.circle_outlined,
-                          color: Colors.grey,
-                          size: 24.0,
-                        ),
-                      ),
-                  ],
-                ),
-              );
-            },
+      body: Stack(children: [
+        GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 2.0,
+            mainAxisSpacing: 2.0,
           ),
-          if (selectedImageCount > 0)
-            Positioned(
-              bottom: 32.0,
-                left: 0.0,
-                right: 0.0,
-                child: Container(
-                  height: 50.0,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border(
-                      top: BorderSide(color: Colors.grey.shade300, width: 1.0),
-                    )
+          itemCount: _imageLinks.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                if (_isSelectMode) {
+                  setState(() {
+                    _selectedImageList[index] = !_selectedImageList[index];
+                  });
+                } else {
+                  context.push('/detail', extra: {
+                    'imageId': photo.imageId,
+                  });
+                }
+              },
+              onLongPress: () {
+                if (!_isSelectMode) {
+                  setState(() {
+                    _isSelectMode = true;
+                  });
+                }
+                setState(() {
+                  _isLongPressed = true;
+                  _selectedImageList =
+                      List.generate(_imageLinks.length, (_) => false);
+                  _selectedImageList[index] = true;
+                  _selectedIndex = index;
+                });
+              },
+              child: Stack(
+                children: [
+                  Image.network(
+                    _imageLinks[index],
+                    fit: BoxFit.cover,
+                    height: MediaQuery.of(context).size.width*0.5,
                   ),
-                  child: Center(
-                    child: Text(
-                      'Delete($selectedImageCount)',
-                      style: const TextStyle(
-                        color: Colors.redAccent,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                  Positioned(
+                    bottom: 4.0,
+                    right: 4.0,
+                    child: _isSelectMode
+                        ? Container(
+                            margin: const EdgeInsets.all(4.0),
+                            child: _selectedImageList[index]
+                                ? const Icon(
+                                    Icons.check_circle,
+                                    color: Colors.blueAccent,
+                                    size: 24.0,
+                                  )
+                                : const Icon(
+                                    Icons.circle_outlined,
+                                    color: Colors.grey,
+                                    size: 24.0,
+                                  ),
+                          )
+                        : const SizedBox(),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        if (selectedImageCount > 0)
+          Positioned(
+            bottom: 32.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              height: 50.0,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border(
+                    top: BorderSide(color: Colors.grey.shade300, width: 1.0),
+                  )),
+              child: Center(
+                child: Text(
+                  'Delete($selectedImageCount)',
+                  style: const TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-            )
-        ]
-      ),
+              ),
+            ),
+          )
+      ]),
     );
   }
 }
