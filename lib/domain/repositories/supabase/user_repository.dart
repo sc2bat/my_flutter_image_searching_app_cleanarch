dcde18cca17a532b801e52bc15c0a0310fb86230
yoonjikimkr <yoonjibiz@gmail.com>
2024-03-11 18:28:47 +0900

    refactor: UserRepository, UserRepositoryImpl - combine updateUserName(), updateUserBio(),updateUserPicture() into updateUserField() #63

--- lib/domain/repositories/supabase/user_repository.dart
+++ lib/domain/repositories/supabase/user_repository.dart
@@ -3,7 +3,5 @@ import 'package:my_flutter_image_searching_app_cleanarch/domain/model/user/user_
 
 abstract interface class UserRepository {
   Future<Result<UserModel>> getUserInfo(String userUuid);
-  Future<Result<void>> updateUserName(String userUuid, String newUserName);
-  Future<Result<void>> updateUserBio(String userUuid, String newUserBio);
-  Future<Result<void>> updateUserPicture(String userUuid, String newUserPicture);
+  Future<Result<void>> updateUserField(String userUuid, String fieldName, String newValue);
 }
