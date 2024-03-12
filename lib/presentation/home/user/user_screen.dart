b594846e25ec721a6718943a466f76a25bea6194
sc2bat <izar_dero@naver.com>
2024-03-13 08:58:13 +0900

    refactor : appbar ontap to home

--- lib/presentation/home/user/user_screen.dart
+++ lib/presentation/home/user/user_screen.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:my_flutter_image_searching_app_cleanarch/data/repositories/supabase/user_repository_impl.dart';
 import 'package:my_flutter_image_searching_app_cleanarch/domain/use_cases/user/user_use_case.dart';
 import 'package:my_flutter_image_searching_app_cleanarch/presentation/home/user/user_view_model.dart';
+import 'package:my_flutter_image_searching_app_cleanarch/presentation/widget/common/title_logo_widget.dart';
 
 import '../../common/theme.dart';
 
@@ -63,21 +64,7 @@ class _UserScreenState extends State<UserScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(
-          'ImageCraft',
-          style: TextStyle(
-            color: baseColor,
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-            shadows: [
-              Shadow(
-                offset: const Offset(2.0, 2.0),
-                blurRadius: 4.0,
-                color: Colors.grey.withOpacity(0.5),
-              ),
-            ],
-          ),
-        ),
+        title: const TitleLogoWidget(),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
